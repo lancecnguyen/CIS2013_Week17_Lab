e1c76bb2ac06deced8008d0c64cb2babf542634b
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-05-08 18:55:24 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,14 @@
 using namespace std;
 
 #define DEBUG false
+
+	
+int add_numbers(int x, int y){
+	return x+=y;
 int main(){
 	
-	bool TRUE;
-	
-	if(DEBUG){ cout << "Value of TRUE: " << TRUE << endl;}
-	
-	cout << "This is something to the screen." << endl;
-	if(TRUE){
-		cout << "...and more to the screen." << endl;
-	} else {
-		cout << "...else less to screen." << endl;
-	}
-	
+	cout << "Adding 5 to 300: " << add_numbers(5,300);
+	cout << "Adding 5 to 300: " << add_numbers(5,300);
+	cout << "Adding 5 to 300: " << add_numbers(5,300);
 	return 0;
 }
