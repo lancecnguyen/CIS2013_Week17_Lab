066a9532151808ef3ef785a7f16035e275c532a1
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-05-08 19:31:43 -0500

    More numbers

--- main.cpp
+++ main.cpp
@@ -13,9 +13,25 @@ int main(){
 	
 	int num1 = 5;
 	int num2 = 300;
+	int *ptr = &num2;
 	
-	cout << "Adding 5 to 300: " << add_numbers(num1,num2) << endl;
-	cout << "Adding 5 to 300: " << add_numbers(num1,num2) << endl;
-	cout << "Adding 5 to 300: " << add_numbers(num1,num2) << endl;
+	cout << "Value of num2 is " << num2 << endl;
+	cout << "value of ptr is " << ptr << endl;
+	num2 = 999;
+	cout << "Value of num2 is " << num2 << endl;
+	cout << "value of ptr is " << ptr << endl;
+	ptr = &num1;
+	cout << "Value of num2 is " << num2 << endl;
+	cout << "value of ptr is " << ptr << endl;
+	num2 = 555;
+	cout << "Value of num2 is " << num2 << endl;
+	cout << "Value of *ptr is " << *ptr	<< endl;
+	cout << "value of ptr is " << ptr << endl;
+	// add_numbers(num1, 1);
+	// cout << "Adding 5 to 300: " << num1 << endl;
+	// add_numbers(num1, num2);
+	// cout << "Adding 5 to 300: " << num1 << endl;
+	// add_numbers(num1, num2);
+	// cout << "Adding 5 to 300: " << num1 << endl;
 	return 0;
 }
